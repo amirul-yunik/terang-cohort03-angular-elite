c4589d5f0365c06ab166e578d2aaeb31ba1ff90f
Amirul Yunik <amirul.yunik@terang.asia>
2018-12-11 16:51:22 +0800

    adding student model

--- src/services/tuition.service.ts
+++ src/services/tuition.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Subject} from '../app/tuition/subject.model';
+import {Student} from '../app/tuition/student.model';
 
 @Injectable()
 export class TuitionService {
@@ -18,4 +19,15 @@ export class TuitionService {
     console.log('This is inside findSubject');
     return this.http.get<Subject>('data/subject.json');
   }
+
+  findStudents(): Observable<Student[]> {
+    console.log('This is inside findStudents');
+    return this.http.get<Student[]>('data/students.json');
+  }
+
+  findStudent(): Observable<Student> {
+    console.log('This is inside findStudent');
+    return this.http.get<Student>('data/student.json');
+  }
+
 }
