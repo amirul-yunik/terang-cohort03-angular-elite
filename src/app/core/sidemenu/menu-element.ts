bc4af07d351b9f848be9b312640b9eb6875c83d3
Amirul Yunik <amirul.yunik@terang.asia>
2018-12-12 00:54:34 +0800

    student list component completed

--- src/app/core/sidemenu/menu-element.ts
+++ src/app/core/sidemenu/menu-element.ts
@@ -6,45 +6,40 @@ export const menus = [
     'open': false,
     'chip': {'value': 1, 'color': 'accent'},
   },
-   /*{
-    'name': 'Tuition',
-    'icon': 'file_copy',
-    'link': false,
+  /*{
+   'name': 'Tuition',
+   'icon': 'file_copy',
+   'link': false,
+   'open': false,
+   'chip': false,
+   'sub': []}
+     */
+  {
+    'name': 'All Subjects',
+    'icon': 'book',
+    'link': '/tuition/subjects/list',
+    'chip': {'value': 2, 'color': 'accent'},
     'open': false,
-    'chip': false,
-    'sub': []}
-      */
-    {
-        'name': 'All Subjects',
-        'icon': 'book',
-        'link': '/tuition/subjects/list',
-        'chip': {'value': 2, 'color': 'accent'},
-        'open': false,
-      },
-      {
-        'name': 'All Instructors',
-        'icon': 'perm_identity',
-        'link': '/academy/apprentices/list',
-        'chip': {'value': 2, 'color': 'accent'},
-        'open': false,
-      },
-      {
-        'name': 'All Students',
-        'icon': 'filter_list',
-        'link': '/tuition/students',
-        'chip': {'value': 2, 'color': 'accent'},
-        'open': false,
-      }
-    ]
   },
-      },
-      {
-        'name': 'All Staffs',
-        'icon': 'perm_identity',
-        'link': '/tuition/staffs/list',
-        'chip': {'value': 2, 'color': 'accent'},
-        'open': true,
-      },
-
-
+  {
+    'name': 'All Instructors',
+    'icon': 'perm_identity',
+    'link': '/academy/apprentices/list',
+    'chip': {'value': 2, 'color': 'accent'},
+    'open': false,
+  },
+  {
+    'name': 'All Students',
+    'icon': 'filter_list',
+    'link': '/tuition/students',
+    'chip': {'value': 2, 'color': 'accent'},
+    'open': false,
+  },
+  {
+    'name': 'All Staffs',
+    'icon': 'perm_identity',
+    'link': '/tuition/staffs/list',
+    'chip': {'value': 2, 'color': 'accent'},
+    'open': true,
+  },
 ];
